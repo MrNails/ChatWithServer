cb231b9335205cc877410ae68182bcc6ca489044
Денис Попович <popov.den30@gmail.com>
2022-02-23 18:12:09 +0200

    Added new type

---
+++ SimpleChatServer.DAL/Models/Restriction.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleChatServer.DAL.Models;
+
+[Flags]
+public enum Restriction
+{
+    None = 0x0000,
+    
+    /// <summary>
+    /// User cannot kick other user
+    /// </summary>
+    NoKicks = 0x0001,
+    
+    /// <summary>
+    /// User cannot send images
+    /// </summary>
+    NoImage = 0x0002,
+    
+    /// <summary>
+    /// User cannot adding new admins
+    /// </summary>
+    NoAddingAdmins = 0x0004,
+    
+    /// <summary>
+    /// User cannot change chat information
+    /// </summary>
+    NoChangingChatInfo = 0x0008,
+}
